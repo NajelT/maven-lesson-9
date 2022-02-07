ccbc8bbdd00830989cde816927bb9b9da239f1a3
Ilja <ilushpajev@gmail.com>
2022-02-07 11:04:46 +0200

    Maven Lesson 9

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.apache.commons.collections4.ListUtils;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        List<String> list1 = new ArrayList<>();
+        list1.add("list1");
+        list1.add("list2");
+
+        List<String> list2 = new LinkedList<>();
+        list2.add("list3");
+
+        System.out.println(list1);
+    }
+
+
+
+
+}
